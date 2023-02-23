d599936ec0aa431cf7a4cbce3ce310cd382beda0
Abhishek <abhishek.magdum.07@gmail.com>
2023-02-23 07:17:25 +0530

    Added: Game between two players

--- src/com/snake_and_ladder/SnakeAndLadder.java
+++ src/com/snake_and_ladder/SnakeAndLadder.java
@@ -4,45 +4,67 @@ import java.util.Random;
 
 public class SnakeAndLadder {
     public static void main(String[] args) {
-        System.out.println("Welcome to Snake and Ladder Programme");
-        System.out.println("Game will start from position 0 ");
-        int position = 0 ;
-        int dieCount = 0 ;
+        int player1Position = 0;
+        int player2Position = 0;
 
-        //Uc2 - The Player rolls the die to get a number between 1 to 6.
-        while (position < 100) {
-            Random random = new Random();
+        Random random = new Random();
+
+        while (player1Position < 100) {
+
             int dice = random.nextInt(6) + 1;
-            System.out.println("Dice Number is = " + dice);
-            dieCount++;
-            System.out.println("Position of Die ="+dieCount);
+            int dice1 = random.nextInt(6) + 1;
+            int step = random.nextInt(3);
 
-            //Uc3 - The Player then checks for Option. They are No Play,Ladder or Snake.
 
-            int move = random.nextInt(3);
-
-            switch (move) {
+            switch (step) {
                 case 0:
-                    System.out.println("Ladder");
-                    position += dice;
+                    System.out.println("No play");
                     break;
                 case 1:
-                    System.out.println("Snake");
-                    position -= dice;
-                    if (position < 0) {
-                        position = 0;
+                    System.out.println("Ladder");
+                    player1Position = player1Position + dice;
+                    player2Position = player2Position + dice1;
+                    if (step == 1) {
+                        player1Position = player1Position + dice;
+                        player2Position = player2Position + dice;
+                    }
+                    if (player1Position > 100) {
+                        player1Position = 100;
+                    }
+                    if (player2Position > 100) {
+                        player2Position = 100;
+                    }
+
+                    break;
+                default:
+                    System.out.println("snake");
+                    player1Position = player1Position - dice;
+                    player2Position = player2Position - dice1;
+                    if (player1Position < 0) {
+                        player1Position = 0;
+                    }
+                    if (player2Position < 0) {
+                        player2Position = 0;
+
                     }
                     break;
-
-                default:
-                    System.out.println("No Play");
             }
-            if(position > 100){
-                position -= dice;
+
+
+            System.out.println("Player 1 position = " + player1Position);
+            System.out.println("player 2 position = " + player2Position);
+
+            if(player1Position > player2Position) {
+                System.out.println("Player 1 is Winner");
+            }else if (player2Position > player1Position) {
+                System.out.println("Player 2 is Winner");
+            }else{
+                System.out.println("Both player score are equal");
             }
         }
-
-        System.out.println("Player Position is = "+position);
-        System.out.println("Numbers of Times Dice was Played = "+dieCount);
     }
 }
+
+
+
+
