730608632e12da2ec99068758874c363bae866d5
Abhishek <abhishek.magdum.07@gmail.com>
2023-02-23 05:30:35 +0530

    Added: Snake and Ladder Program

---
+++ src/com/snake_and_ladder/SnakeAndLadder.java
@@ -0,0 +1,10 @@
+package com.snake_and_ladder;
+
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake and Ladder Programme");
+        System.out.println("Game will start from position 0 ");
+        int startPosition = 0 ;
+    }
+
+}
